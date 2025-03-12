5cd68a7ae231ca17f3f64159425a5cb5e2150284
= <jeffreypincombe@gmail.com>
2025-03-12 10:55:21 -0400

    Added Missile Limit
    Missiles now destroy when they go off screen and there can only be two at a time

--- Galaga (3D Programming)/Assets/SCRIPTS/MissileScript.cs
+++ Galaga (3D Programming)/Assets/SCRIPTS/MissileScript.cs
@@ -4,15 +4,19 @@ public class MissileScript : MonoBehaviour
 {   [SerializeField] private Rigidbody rigidbody;
     [SerializeField] private float Speed;
     
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
+//you need to hide the scenview in unity for this to work properly.
+//If yhe sceneview can see it, the object wont be destroyed
     void OnBecameInvisible()
     {
         Destroy(gameObject);
+       
+        
     }
     // Update is called once per frame
     void Update()
--- Galaga (3D Programming)/Assets/SCRIPTS/SpawnerLogic.cs
+++ Galaga (3D Programming)/Assets/SCRIPTS/SpawnerLogic.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnerLogic : MonoBehaviour
 {
+    public static SpawnerLogic Instance {get; set;}
     [SerializeField] private GameObject _obj;
     [SerializeField] private KeyCode _key;
+    public List<GameObject> numOfMissiles = new List<GameObject>();
+    //var numberOfMissiles = GameObject.FindGameObjectWithTag("Missile").Length;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,9 +18,14 @@ public class SpawnerLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        numOfMissiles.RemoveAll(item => item == null);
+        if (numOfMissiles.Count < 2){
         if (Input.GetKeyDown(_key) == true)
         {
-            Instantiate(_obj, this.transform.position, this.transform.rotation);
+           GameObject newMissile = Instantiate(_obj, this.transform.position, this.transform.rotation);
+            numOfMissiles.Add(newMissile);
+        }
+        
         }
     }
 }
